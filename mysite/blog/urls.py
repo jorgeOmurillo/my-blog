7161f5907c8d00fe225e663ab9fd27a0b1870aa0
Jorge Murillo <j.orlandomurillo@gmail.com>
2017-08-02 18:43:10 +0800

    deleted:    ../../blog/blog/__init__.py 	deleted:    ../../blog/blog/settings.py 	deleted:    ../../blog/blog/urls.py 	deleted:    ../../blog/blog/wsgi.py 	deleted:    ../../blog/manage.py 	deleted:    ../../manage.py 	modified:   ../blog/templates/blog/base.html 	modified:   ../blog/templates/blog/post_title.html 	modified:   ../blog/urls.py 	modified:   ../blog/views.py 	../../mysite-ENV/ 	../blog/forms.py 	../blog/templates/blog/post_new.html

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
         url(r'^$', views.post_list, name='post_list'),
         url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
         url(r'^title/$', views.post_title, name='post_title'),
+        url(r'^post/new/$', views.post_new, name='post_new'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -13,4 +14,18 @@ def post_detail(request, pk):
 
 def post_title(request):
     title = Post.objects.all()
-    return render(request, 'blog/post_title.html', {'post': title})
+    return render(request, 'blog/post_title.html', {'title': title})
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)    
+        
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_new.html', {'form': form})
