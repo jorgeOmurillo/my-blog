f16abe80da9f80770ad04265d7d77256d9472971
Jorge Murillo <j.orlandomurillo@gmail.com>
2017-08-02 19:35:46 +0800

    updates

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
         url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
         url(r'^title/$', views.post_title, name='post_title'),
         url(r'^post/new/$', views.post_new, name='post_new'),
+        url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
