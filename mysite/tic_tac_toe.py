4ef9aefa0980709f7a4eb505a9f2d730fbe5ad58
jorgeOmurillo <j.orlandomurillo@gmail.com>
2017-08-03 20:12:34 +0800

    updates:

--- mysite/tic_tac_toe.py
+++ mysite/tic_tac_toe.py
@@ -1,14 +1,21 @@
+def print_grid():
+    print(" " + str(grid[0]) + " | " + str(grid[1]) + " | " + str(grid[2]))
+    print("-----------")
+    print(" " + str(grid[3]) + " | " + str(grid[4]) + " | " + str(grid[5]))
+    print("-----------")
+    print(" " + str(grid[6]) + " | " + str(grid[7]) + " | " + str(grid[8]))
+
+def win_or_lose(end):
+    if grid[0] == 'X' and grid[1] == 'X' and grid[2] =='X':
+        print("Player 1 wins!")
+        end = True
+
+grid = [' ']*9
+
 end = False
 
-def print_grid(num):
-    print("  | " + str(grid[num]) + " | " + " |  ")
-    print("-----------")
-    print("  | " + " | " + " |  ")
-    print("-----------")
-    print("  | " + " | " + " |  ")
-
-grid = [0, 1, 2, 3 , 4 , 5, 6, 7, 8, 9]
-
-while end is not True: 
+while end is not True:
     p1 = int(input("input your option player 1\n"))
-    print_grid(p1)
+    grid[p1] = 'X'
+    win_or_lose(end)
+    print_grid()
