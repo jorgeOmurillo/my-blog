6029c21a1896929a8bc0eb9185a9805e3cf2ac33
jorgeOmurillo <j.orlandomurillo@gmail.com>
2017-08-06 21:45:44 +0800

    updates

--- mysite/tic_tac_toe.py
+++ mysite/tic_tac_toe.py
@@ -6,83 +6,83 @@ def print_grid():
     print(" " + str(grid[6]) + " | " + str(grid[7]) + " | " + str(grid[8]))
 
 def win_or_lose():
-    if grid[0] == 'X' and grid[1] == 'X' and grid[2] =='X':
+    if grid[0] and grid[1] and grid[2] =='X':
         print("Player 1 wins!")
         end = True
         return end
 
-    if grid[3] == 'X' and grid[4] == 'X' and grid[5] =='X':
+    if grid[3] and grid[4] and grid[5] =='X':
         print("Player 1 wins!")
         end = True
         return end
 
-    if grid[6] == 'X' and grid[7] == 'X' and grid[8] =='X':
+    if grid[6] and grid[7] and grid[8] =='X':
         print("Player 1 wins!")
         end = True
         return end
 
 
-    if grid[0] == 'X' and grid[3] == 'X' and grid[6] =='X':
+    if grid[0] and grid[3] and grid[6] =='X':
         print("Player 1 wins!")
         end = True
         return end
 
-    if grid[1] == 'X' and grid[4] == 'X' and grid[7] =='X':
+    if grid[1] and grid[4] and grid[7] =='X':
         print("Player 1 wins!")
         end = True
         return end
 
-    if grid[2] == 'X' and grid[5] == 'X' and grid[8] =='X':
+    if grid[2] and grid[5] and grid[8] =='X':
         print("Player 1 wins!")
         end = True
         return end
 
-    if grid[0] == 'X' and grid[4] == 'X' and grid[8] =='X':
+    if grid[0] and grid[4] and grid[8] =='X':
         print("Player 1 wins!")
         end = True
         return end
 
-    if grid[2] == 'X' and grid[4] == 'X' and grid[6] =='X':
+    if grid[2] and grid[4] and grid[6] =='X':
         print("Player 1 wins!")
         end = True
         return end
 
-    if grid[0] == 'O' and grid[1] == 'O' and grid[2] =='O':
+    if grid[0] and grid[1] and grid[2] =='O':
         print("Player 2 wins!")
         end = True
         return end
 
-    if grid[3] == 'O' and grid[4] == 'O' and grid[5] =='O':
+    if grid[3] and grid[4] and grid[5] =='O':
         print("Player 2 wins!")
         end = True
         return end
 
-    if grid[6] == 'O' and grid[7] == 'O' and grid[8] =='O':
+    if grid[6] and grid[7] and grid[8] =='O':
         print("Player 2 wins!")
         end = True
         return end
 
-    if grid[0] == 'O' and grid[3] == 'O' and grid[6] =='O':
+    if grid[0] and grid[3] and grid[6] =='O':
         print("Player 2 wins!")
         end = True
         return end
 
-    if grid[1] == 'O' and grid[4] == 'O' and grid[7] =='O':
+    if grid[1] and grid[4] and grid[7] =='O':
         print("Player 2 wins!")
         end = True
         return end
 
-    if grid[2] == 'O' and grid[5] == 'O' and grid[8] =='O':
+    if grid[2] and grid[5] and grid[8] =='O':
         print("Player 2 wins!")
         end = True
         return end
 
-    if grid[0] == 'O' and grid[4] == 'O' and grid[8] =='O':
+    if grid[0] and grid[4] and grid[8] =='O':
         print("Player 2 wins!")
         end = True
         return end
 
-    if grid[2] == 'O' and grid[4] == 'O' and grid[6] =='O':
+    if grid[2] and grid[4] and grid[6] =='O':
         print("Player 2 wins!")
         end = True
         return end
@@ -98,8 +98,12 @@ while end is not True:
     grid[p1] = 'X'
     end = win_or_lose()
     print_grid()
+    if end == True:
+        break
 
     p2 = int(input("input your option player 2\n"))
     grid[p2] = 'O'
     end = win_or_lose()
     print_grid()
+    if end == True:
+        break
